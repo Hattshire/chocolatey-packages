525e6f5ddcdfd602efe3f63156eacfc6d41be981
hattshire <hattshire@gmail.com>
2023-10-05 17:53:45 -0300

    SquirrelDisk: Updated t0 v0.3.4

--- squirreldisk/tools/chocolateyinstall.ps1
+++ squirreldisk/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url64bit       = 'https://github.com/adileo/squirreldisk/releases/download/v0.3.0/SquirrelDisk_0.3.0_x64_en-US.msi'
-  checksum64     = ''
+  url64bit       = 'https://github.com/adileo/squirreldisk/releases/download/v0.3.4/SquirrelDisk_0.3.4_x64_en-US.msi'
+  checksum64     = '82b3bff621aa871e19d5fd2fa25c303cf2d5c0a67b1c1d18df6d51c0e80d27a1'
   checksumType64 = 'sha256'
 
   softwareName   = 'SquirrelDisk*'
