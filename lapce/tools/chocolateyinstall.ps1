8ded3e11e3a5a7d6c1f5aeb62ea2ed981e837b37
hattshire <hattshire@gmail.com>
2023-10-05 17:53:08 -0300

    Lapce: Updated to v0.2.8

--- lapce/tools/chocolateyinstall.ps1
+++ lapce/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url64bit       = 'https://github.com/lapce/lapce/releases/download/v0.2.7/Lapce-windows.msi'
-  checksum64     = 'b9738f4dea88394f62c6a97b9ba0ccaef8e39ecfa175a3d57e2b1ba82bdc46d0'
+  url64bit       = 'https://github.com/lapce/lapce/releases/download/v0.2.8/Lapce-windows.msi'
+  checksum64     = 'e45ba8c907c340b7ad5c94bff94ecf4340f4a65d6005b868bc35a999b45d4766'
   checksumType64 = 'sha256'
 
   softwareName   = 'lapce*'
