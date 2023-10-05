f0997e8ff029312e02b15dfc3af2f5bc1bcacde8
hattshire <hattshire@gmail.com>
2023-10-05 17:44:55 -0300

    SquirrelDisk: Added

---
+++ squirreldisk/Readme.md
@@ -0,0 +1,5 @@
+# SquirrelDisk: Beautiful, Cross-Platform and Super Fast Disk Usage Analysis Tool
+## What's taking your hard disk space?
+
+The easiest open source app you will ever use to detect huge files. Built with Rust + React (Tauri).
+Squirreldisk is an open source alternative to softwares like: WinDirStat, WizTree, TreeSize and DaisyDisk.
---
+++ squirreldisk/tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿
+$ErrorActionPreference = 'Stop'
+
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'msi'
+  url64bit       = 'https://github.com/adileo/squirreldisk/releases/download/v0.3.0/SquirrelDisk_0.3.0_x64_en-US.msi'
+  checksum64     = ''
+  checksumType64 = 'sha256'
+
+  softwareName   = 'SquirrelDisk*'
+
+  silentArgs     = "/quiet"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
